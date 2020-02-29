e402612ee1a52c14eee2d3a07a79f2c3c568b4b1
Suzuha Amane <60668433+labmem008@users.noreply.github.com>
2020-02-29 02:55:13 -0500

    Update Problem3.cpp

--- Problem3.cpp
+++ Problem3.cpp
@@ -8,8 +8,11 @@ int lengthOfLongestSubstring(string s) {
 	int ans = 0;
 	int n = s.size(), left = -1;
 	for (int i = 0; i < n; i++) {
-		if (map.count(s[i]) && map[s[i]] > left)	// map.count(key1) return 0 if not found key1 in map, return 1 if found key1 in map
-			left = map[s[i]];	// map[key1] return the value of key1, in this case, it stores the starting index of our substring
+		// map.count(key1) return 0 if not found key1 in map, 
+		// return 1 if found key1 in map
+		if (map.count(s[i]) && map[s[i]] > left)	
+			// map[key1] return the value of key1, in this case, it stores the starting index of our substring
+			left = map[s[i]];	
 		map[s[i]] = i;
 		ans = max(ans, i - left);
 	}
